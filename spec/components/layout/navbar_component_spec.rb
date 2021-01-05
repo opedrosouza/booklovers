f17d2581eef365e6bd99bb90ffa291e666ccb71c
Pedro Souza <opedrosouza@hotmail.com>
2021-01-05 16:18:23 -0300

    Added alert component to show messages, improve root page, write tests

--- spec/components/layout/navbar_component_spec.rb
+++ spec/components/layout/navbar_component_spec.rb
@@ -1,11 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Layout::Navbar::Component, type: :view do
-  subject(:component) { described_class.new }
-
-  it 'render navbar' do
-    render(component)
-    expect(rendered).to have_link("BookLovers", href: root_path)
+  describe 'Render with logged admin' do
+    subject(:component) { described_class.new(user: create(:admin)) }
+    it 'render navbar' do
+      render(component)
+      expect(rendered).to have_link("Novo Livro", href: new_book_path)
+    end
   end
+
+  describe 'Render without logged admin' do
+    subject(:component) { described_class.new() }
+    it 'render navbar' do
+      render(component)
+      expect(rendered).to have_link("BookLovers", href: root_path)
+    end
+  end
+  
   
 end
