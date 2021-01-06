059546768b36fef4bfac325f0f915f0cbcb57944
Pedro Souza <opedrosouza@hotmail.com>
2021-01-06 00:47:57 -0300

    Added devise member group to improve the write of code to check if is admin or user

--- app/components/layout/navbar/component.rb
+++ app/components/layout/navbar/component.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class Layout::Navbar::Component < ViewComponent::Base
-  def initialize(user: nil)
-    @user = user
+  def initialize(member: nil)
+    @member = member
+  end
+
+  def is_user?
+    @member.class == User
+  end
+
+  def is_admin?
+    @member.class == Admin
   end
 end
--- spec/components/layout/navbar_component_spec.rb
+++ spec/components/layout/navbar_component_spec.rb
@@ -2,14 +2,22 @@ require "rails_helper"
 
 RSpec.describe Layout::Navbar::Component, type: :view do
   describe 'Render with logged admin' do
-    subject(:component) { described_class.new(user: create(:admin)) }
+    subject(:component) { described_class.new(member: create(:admin)) }
     it 'render navbar' do
       render(component)
       expect(rendered).to have_link("Novo Livro", href: new_book_path)
     end
   end
 
-  describe 'Render without logged admin' do
+  describe 'Render with logged user' do
+    subject(:component) { described_class.new(member: create(:user)) }
+    it 'render navbar' do
+      render(component)
+      expect(rendered).to have_link("Sair", href: destroy_user_session_path)
+    end
+  end
+
+  describe 'Render without logged member' do
     subject(:component) { described_class.new() }
     it 'render navbar' do
       render(component)
